13d979cd2af31d9a92402db19f2a6c96c1b253f8
Norx-Nishidh47 <nishidhsingh97@gmail.com>
2024-07-05 23:18:31 +0530

    completed

---
+++ que-1.py
@@ -0,0 +1,18 @@
+import mysql.connector as sql
+con=sql.connect(host="localhost",username="root",password="nishidh@123",database="SCHOOL")
+cur=con.cursor()
+
+def init():
+    enteries=int(input("Enter the no. of entries : "))
+    for i in range(enteries):
+        global query
+        query=f"insert into Student (rno,name,DOB,fee) values ({int(input('Enter Roll NO :'))},'{str(input('Enter the name of the student : '))}','{str(input("Enter the date of Admission in (YYYY-MM-DD) : "))}',{float(input("Enter the fee of the student : "))});"
+        cur.execute(query)
+        con.commit()
+init()
+if cur.rowcount!=None:
+    print("Successfully executed")
+else:
+    print("try again")
+    init()    
+print(query)
