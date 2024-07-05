7ff377a73ce462098c140c8afdc1990ef6d2578d
Norx-Nishidh47 <nishidhsingh97@gmail.com>
2024-07-05 23:25:20 +0530

    entery changes

--- que-1.py
+++ que-1.py
@@ -2,8 +2,7 @@ import mysql.connector as sql
 con=sql.connect(host="localhost",username="root",password="nishidh@123",database="SCHOOL")
 cur=con.cursor()
 
-def init():
-    enteries=int(input("Enter the no. of entries : "))
+def init(enteries):
     for i in range(enteries):
         global query
         query=f"insert into Student (rno,name,DOB,fee) values ({int(input('Enter Roll NO :'))},'{str(input('Enter the name of the student : '))}','{str(input("Enter the date of Admission in (YYYY-MM-DD) : "))}',{float(input("Enter the fee of the student : "))});"
